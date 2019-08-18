2e4d113fe1f57bb67b7f6840b4f9be41517b5db6
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-08-18 15:33:48 -0600

    added server.js and connection.js code

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO allburgers (name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO allburgers (name, devoured) VALUES ('Peanut Butter and Jelly Burger', false);
+INSERT INTO allburgers (name, devoured) VALUES ('California Burger', true);
+INSERT INTO allburgers (name, devoured) VALUES ('Fried Egg Burger', true);
+INSERT INTO allburgers (name, devoured) VALUES ('Chilli Cheeseburger', false);
