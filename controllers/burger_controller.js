14279d61b8258a2a87dfa368191c0296d509626b
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-08-26 23:52:13 -0600

    changed name to burger_name

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -21,8 +21,8 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
   burger.create([
-    "name"], [
-      req.body.name
+    "burger_name"], [
+      req.body.burger_name
     ], function (result) {
       // Send back the ID of the new quote
       // res.json({ id: result.insertId });
